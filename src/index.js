f940acec42a0822c0a0bff726384523207810b9e
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-06-03 09:08:58 +0900

    Feat: get param

--- src/index.js
+++ src/index.js
@@ -2,15 +2,28 @@ import http from "http";
 import fs from "fs";
 
 const app = http.createServer((request, response) => {
-  let url = request.url;
-  if (request.url === "/") {
-    url = "/src/view/index.html";
+  let _url = request.url;
+  console.log(_url);
+
+  // const queryString = URLClass.queryString;
+  // console.log(queryString);
+
+  if (_url === "/") {
+    _url = "/src/view/index.html";
+    const URLClass = new URL("http://localhost:3000/?name=raven");
+    console.log(URLClass.searchParams.get("name"));
+
+    //MEMO: Legacy Code
+    // const queryData = url.parse(_url, true).query;
+    // console.log(queryData.id);
   }
 
-  if (request.url === "/favicon.ico") return response.writeHead(404);
+  if (_url === "/favicon.ico") return response.writeHead(404);
 
   response.writeHead(200);
-  response.end(fs.readFileSync(process.cwd() + url));
+  console.log(process.cwd());
+  console.log(__dirname);
+  response.end(fs.readFileSync(process.cwd() + _url));
 });
 
 const PORT = 3000;
