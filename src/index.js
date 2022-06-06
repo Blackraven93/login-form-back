3497dd290fda2e5d4ff53ab9f24f21b48b39ac87
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-06-06 19:35:10 +0900

    Feat: handle file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import fs from "fs";
 const app = http.createServer((request, response) => {
   let _url = request.url;
   console.log(_url);
+
   if (_url === "/") {
     _url = "/src/view/index.html";
     response.writeHead(200);
@@ -21,8 +22,31 @@ const app = http.createServer((request, response) => {
   const { host } = request.headers;
   const URLClass = new URL(`http://${host}${_url}`);
   const name = URLClass.searchParams.get("name");
-  const template = `<h1>bye bye~!!!!! ${name}</h1>`;
-  response.end(template);
+
+  // readFile
+  const text = fs.readFile(
+    process.cwd() + `/src/view/${name}.txt`,
+    "utf8",
+    (err, description) => {
+      const template = `
+      <!DOCTYPE html>
+      <html lang="ko">
+        <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Document</title>
+        </head>
+        <body>
+          <h1>${name}</h1>
+          <p>${description}</p>
+        </body>
+      </html>
+      `;
+
+      response.end(template);
+    }
+  );
 });
 
 const PORT = 3000;
